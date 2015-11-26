86c1652a6d01df3782e8451d6466f936d9c18947
Kristjan Ribolica <riba1122@gmail.com>
2015-11-26 02:36:51 +0100

    added few libraries

--- main.R
+++ main.R
@@ -2,6 +2,9 @@ library(CORElearn)
 library(plyr)
 library(rpart)
 library(adabag)
+library(e1071) # SVM https://cran.r-project.org/web/packages/e1071/e1071.pdf
+library(tictoc)
+
 source("helpers.R")
 source("exploration.R")
 source("functions.R")
