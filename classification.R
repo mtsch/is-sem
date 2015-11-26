ab17998699f5ec1bb5c2c6865e62bccc16373887
Kristjan Ribolica <riba1122@gmail.com>
2015-11-26 02:35:45 +0100

    tried SVM, classified using different data

--- classification.R
+++ classification.R
@@ -1,10 +1,17 @@
-test_sample <- sample(nrow(clean),500)
-test = clean[test_sample,]
-train = clean[-test_sample,]
+clean2 <- clean
+clean2 <- excludeByColumn(clean2,c("YEAR","DAY","TLONG","RAIN"))
+clean2 <- excludeByColumn(clean2,c("YDAY"))
+test_sample <- sample(nrow(clean2),500)
+test = clean2[test_sample,]
+train = clean2[-test_sample,]
 majority_classifier <- nrow(all[all$OZONE_CLASS == names(which.max(table(all$OZONE_CLASS))),]) / nrow(all)
 
+tmp <- sampleClassesEqualDist(clean2,"OZONE_CLASS")
+train <- tmp$train
+test <- tmp$test
 
 
+tic
 models <- c("tree","rf","bayes","knn")
 ests <- c("MDL","InfGain","GainRatio","ReliefFequalK","ReliefFexpRank")
 accuracies <- data.frame()
@@ -19,18 +26,39 @@ for (i in models){ #needs 5-10 minutes to run
     }
     
 }
+toc
+# load("accuracies_clean_data.Rda") #load accuracies from upper loops
 
-load("accuracies_clean_data.Rda") #load accuracies from upper loops
 
-
-boost_model <- boosting(OZONE_CLASS ~ .,train)
+boost_model <- boosting(OZONE_CLASS ~ .,train, mfinal = 100)
 pred <- predict.boosting(boost_model, test)
 accuracy <- sum(pred$class == test$OZONE_CLASS) / nrow(test)
+accuracy
 # 0.842 clean data
+# 0.8364929 uniform dist clean data
+# 0.8388626 removed YDAY
+# 0.84 clean removed DAY YEAR RAIN TLONG
+# 0.8246445 uniform dist removed DAY YEAR RAIN TLONG
 
-
-bagg_model <- bagging(OZONE_CLASS ~ ., train)
+bagg_model <- bagging(OZONE_CLASS ~ ., train, mfinal = 100)
 pred <- predict.bagging(bagg_model,test)
 accuracy <- sum(pred$class == test$OZONE_CLASS) / nrow(test)
+accuracy
 # 0.826 clean data
+# 0.8151659 uniform dist clean data
+# 0.8341232 removed YDAY 
+# 0.838 clean removed DAY YEAR RAIN TLONG
+# 0.8127962 uniform dist removed DAY YEAR RAIN TLONG
+
+svm_model <- svm(OZONE_CLASS ~ ., train, scale=TRUE, kernel="polynomial")
+pred <- predict(svm_model, test)
+accuracy <- modelEval(model=NULL,test$OZONE_CLASS,pred) # model is null because it's not a CORElearn model
+accuracy[1:8]
+# 0.0.812 clean data 
+# 0.0.7654028 uniform dist clean data # wrong, used clean as train
+# 0.812 clean removed DAY YEAR RAIN TLONG
+# 0.8104265 uniform dist removed DAY YEAR RAIN TLONG
+
+
+
 
