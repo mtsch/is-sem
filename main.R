a83c4b53939066fef8ed217e44b16861bfb5fa12
Kristjan Ribolica <riba1122@gmail.com>
2015-11-27 03:17:41 +0100

    added perceptron library and comments

--- main.R
+++ main.R
@@ -1,9 +1,10 @@
-library(CORElearn)
+library(CORElearn) # tree, knn, bayes, rf, regtree https://cran.r-project.org/web/packages/CORElearn/CORElearn.pdf
 library(plyr)
 library(rpart)
-library(adabag)
-library(e1071) # SVM https://cran.r-project.org/web/packages/e1071/e1071.pdf
+library(adabag) # boosting and bagging    https://cran.r-project.org/web/packages/e1071/e1071.pdf
+library(e1071) # SVM    https://cran.r-project.org/web/packages/e1071/e1071.pdf
 library(tictoc)
+library(monmlp) # multilayer perceptron    https://cran.r-project.org/web/packages/monmlp/monmlp.pdf
 
 source("helpers.R")
 source("exploration.R")
